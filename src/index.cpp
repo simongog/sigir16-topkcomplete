20aa0562fcfac4ab6239b052ffd7e498496f4bb4
Simon Gog <simon.gog@gmail.com>
2016-07-01 22:53:52 +0200

    Parse input

--- src/index.cpp
+++ src/index.cpp
@@ -1,6 +1,7 @@
 #include "topkcomp/index1.hpp"
 #include <cstdio>
 #include <iostream>
+#include <string>
 
 using namespace std;
 using namespace sdsl;
@@ -8,5 +9,13 @@ using namespace sdsl;
 int main(int argc, char* argv[]){
     if ( argc < 2 ) {
         cout << "Usage: ./" << argv[1] << " file" << endl;
+    }
+    ifstream in(argv[1]);
+    string entry;
+    while ( getline(in, entry, '\t')  ) {
+        string s_priority;
+        getline(in, s_priority);
+        uint64_t priority = stoull(s_priority);
+        cout << "(" << entry << ", " << priority << ")" << endl;
     }
 }
