9d7fa7b6f9b3710681d14a7d576ef04cfd2309ab
Simon Gog <simon.gog@gmail.com>
2016-07-02 17:35:12 +0200

    Added timing

--- src/index.cpp
+++ src/index.cpp
@@ -3,15 +3,18 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <chrono>
 
 using namespace std;
 using namespace sdsl;
 using namespace topkcomp;
 
+
 int main(int argc, char* argv[]){
     if ( argc < 2 ) {
         cout << "Usage: ./" << argv[1] << " file" << endl;
     }
+    using clock = std::chrono::high_resolution_clock;
     ifstream in(argv[1]);
     tVSI entry_priority;
     string entry;
@@ -28,11 +31,16 @@ int main(int argc, char* argv[]){
 
     string prefix;
     while ( getline(cin, prefix) ) {
+        auto query_start = clock::now();
         auto result_list = idx.top_k(prefix, 5);
+        auto query_time  = clock::now() - query_start;
+        auto query_us    = chrono::duration_cast<chrono::microseconds>(query_time).count();
+        
         cout << "-- top results:" << endl;
         for (size_t i=0; i<result_list.size(); ++i) {
             cout << result_list[i].first << "  " << result_list[i].second << endl;
         }
-        cout << "--" << endl;
+        cout << "-- (" << std::setprecision(3) << query_us / 1000.0;
+        cout << " ms)" << endl;
     }
 }
