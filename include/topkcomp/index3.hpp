8770f5eb842005a3005bb8a0ba3a6259634a215c
Simon Gog <simon.gog@gmail.com>
2016-07-04 10:11:48 +0200

    Added all required members

--- include/topkcomp/index3.hpp
+++ include/topkcomp/index3.hpp
@@ -2,6 +2,7 @@
 
 #include "index_common.hpp"
 #include <sdsl/bit_vectors.hpp>
+#include <sdsl/bp_support.hpp>
 #include <algorithm>
 #include <queue>
 #include <locale> // required by std::tolower
@@ -9,10 +10,16 @@
 namespace topkcomp {
 
 template<typename t_bv = sdsl::bit_vector,
-         typename t_sel= typename t_bv::select_1_type>
+         typename t_sel= typename t_bv::select_1_type,
+         typename t_bp_support = sdsl::bp_support_sada<>,
+         typename t_bp_rnk10 = sdsl::rank_support_v<10,2>,
+         typename t_bp_sel10 = sdsl::select_support_mcl<10,2>>
 class index3 {
     sdsl::int_vector<8> m_labels;  // labels of the tree
     sdsl::bit_vector    m_bp; // balanced parantheses sequence of tree
+    t_bp_support        m_bp_support; // support structure for m_bp
+    t_bp_rnk10          m_bp_rnk10;  // rank for leaves in bp
+    t_bp_sel10          m_bp_sel10;  // select for leaves in bp
     t_bv                m_start_bv;  // bitvector which represents the start of labels in m_text
     t_sel               m_start_sel; // select structure for m_start_bv
     sdsl::int_vector<>  m_priority;  //
@@ -20,13 +27,14 @@ class index3 {
     public:
         typedef size_t size_type;
 
-        void build_tree(const tVSI& entry_priority) {
-            build_tree(entry_priority, 0, entry_priority.size(), 0);
+        void build_tree(const tVSI& entry_priority, size_t& bp_idx, size_t &start_idx, size_t &label_idx) {
+            build_tree(entry_priority, 0, entry_priority.size(), 0, bp_idx, start_idx, label_idx);
         }
 
-        void build_tree(const tVSI& entry_priority, size_t lb, size_t rb, size_t depth) {
+        void build_tree(const tVSI& entry_priority, size_t lb, size_t rb, size_t depth, size_t& bp_idx, size_t& start_idx, size_t& label_idx) {
             if ( lb >= rb )
                 return;
+            m_bp[bp_idx++] = 1; // append ,,(''
             std::cout << "(";
 //            std::cout<<depth<<"-["<<lb<<","<<rb-1<<"]"<<std::endl;
             size_t d = depth;
@@ -34,8 +42,11 @@ class index3 {
             const char* rb_entry = entry_priority[rb-1].first.c_str();
             while ( lb_entry[d] !=0 and lb_entry[d] == rb_entry[d] ) {
                 std::cout << lb_entry[d];
+                m_labels[label_idx++] = lb_entry[d];
+                start_idx++;
                 ++d;
             }
+            m_start_bv[start_idx++] = 1; 
             if ( lb_entry[d] == 0 ) {
                 ++lb;
             }
@@ -55,17 +66,36 @@ class index3 {
                     ++mid;
                 }
 //                std::cout<<"["<<lb<<","<<mid-1<<"_"<<rb<<"] d="<<d<<std::endl;
-                build_tree(entry_priority, lb, mid, d);
+                build_tree(entry_priority, lb, mid, d, bp_idx, start_idx, label_idx);
                 lb = mid;
             }
             std::cout << ")";
+            m_bp[bp_idx++] = 0; // append ,,)''
         }
 
         // Constructor
         index3(const tVSI& entry_priority=tVSI()) {
-            std::stack<size_t> depth;
-
-            build_tree(entry_priority);
+            // get the length of the concatenation of all strings
+            uint64_t n = std::accumulate(entry_priority.begin(), entry_priority.end(),
+                            0, [](uint64_t a, std::pair<std::string, uint64_t> ep){
+                                    return a + ep.first.size();
+                               });
+            // m_bp size is at most 2*2*N
+            // 
+            size_t N   = entry_priority.size(), bp_idx=0, start_idx = 0, label_idx=0;
+            m_labels   = sdsl::int_vector<8>(n);
+            m_start_bv = sdsl::bit_vector(N+n+2, 0);
+            m_bp       = sdsl::bit_vector(2*2*N, 0);
+            m_start_bv[start_idx++] = 1;
+            build_tree(entry_priority, bp_idx, start_idx, label_idx);
+            m_bp.resize(bp_idx);
+            m_labels.resize(label_idx);
+            m_start_bv.resize(start_idx);
+            std::cout<<"m_bp="<<m_bp<<std::endl;
+            std::cout<<"m_labels=";
+            for(char c : m_labels ) std::cout << c;
+            std::cout<<std::endl;
+            std::cout<<"m_start_bv="<<m_start_bv<<std::endl;
         }
 
         // k > 0
