cbe82186cd078f4307e0a3cf5e55a8f853aa832c
Simon Gog <simon.gog@gmail.com>
2016-07-12 11:02:27 +0200

    download file

--- data/download.cmake
+++ data/download.cmake
@@ -1,8 +1,12 @@
 SET(base_url "http://xor.di.unipi.it/~rossano")
 
-SET(abs_test_case ${CMAKE_HOME_DIRECTORY}/data/${test_case})
+MESSAGE("test_case=${test_case}")
+SET(abs_test_case ${CMAKE_CURRENT_SOURCE_DIR}/${test_case})
+MESSAGE("abs_test_case=${abs_test_case}")
+
 IF(NOT EXISTS ${abs_test_case})
     SET(tc_url ${base_url}/${test_case})
     MESSAGE(${tc_url})
     FILE(DOWNLOAD ${tc_url} ${abs_test_case} SHOW_PROGRESS)
 ENDIF()
+MESSAGE("Download complete; unzipping file...")
